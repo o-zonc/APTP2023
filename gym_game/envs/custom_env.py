2a4760239964ea67ec0f5a0c8fdd53d7a06d9c2e
o-zonc <i8777964@gmail.com>
2023-06-27 18:20:59 +0900

    last. maybe

---
+++ gym_game/envs/custom_env.py
@@ -0,0 +1,28 @@
+import gym
+from gym import spaces
+import numpy as np
+from gym_game.envs.pygame_2d import PyGame2D
+
+
+class CustomEnv(gym.Env):
+    # metadata = {'render.modes' : ['human']}
+    def __init__(self):
+        self.pygame = PyGame2D()
+        self.action_space = spaces.Discrete(3)
+        self.observation_space = spaces.Box(np.array([0, 0, 0, 0, 0]), np.array([10, 10, 10, 10, 10]), dtype=np.int)
+
+    def reset(self):
+        del self.pygame
+        self.pygame = PyGame2D()
+        obs = self.pygame.observe()
+        return obs
+
+    def step(self, action):
+        self.pygame.action(action)
+        obs = self.pygame.observe()
+        reward = self.pygame.evaluate()
+        done = self.pygame.is_done()
+        return obs, reward, done, {}
+
+    def render(self, mode="human", close=False):
+        self.pygame.view()
